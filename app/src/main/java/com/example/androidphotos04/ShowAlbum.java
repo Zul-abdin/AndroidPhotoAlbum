1a5a7ebbe8380126a19cae90b5199888f396f966
Zul-abdin <zulabdin4463@gmail.com>
2019-11-28 16:14:52 -0500

    Add album.xml and album_list.xml menu_main has add button to add album (Not implemented) gradle uses java level 8 instead of 7

---
+++ app/src/main/java/com/example/androidphotos04/ShowAlbum.java
@@ -0,0 +1,14 @@
+package com.example.androidphotos04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowAlbum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.albums_list);
+    }
+}
